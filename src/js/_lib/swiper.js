8a2a200bf94f8c9d31bbdf08bbb5cd2eb13fda16
MrHidalgo <Luckyhidalgo@gmail.com>
2018-10-03 16:13:52 +0300

    Singer section - full adaptive.

--- src/js/_lib/swiper.js
+++ src/js/_lib/swiper.js
@@ -95,23 +95,21 @@ const initSwiper = () => {
     controller: {
       control: mySwiperView,
     },
-    // breakpoints: {
-    //   // when window width is <= 320px
-    //   320: {
-    //     slidesPerView: 1,
-    //     spaceBetween: 10
-    //   },
-    //   // when window width is <= 480px
-    //   480: {
-    //     slidesPerView: 2,
-    //     spaceBetween: 20
-    //   },
-    //   // when window width is <= 640px
-    //   640: {
-    //     slidesPerView: 3,
-    //     spaceBetween: 30
-    //   }
-    // },
+    breakpoints: {
+      // // when window width is <= 320px
+      // 320: {
+      //   slidesPerView: 1,
+      //   spaceBetween: 10
+      // },
+      // when window width is <= 575
+      575: {
+        slidesPerView: 2
+      },
+      // when window width is <= 767
+      767: {
+        slidesPerView: 3
+      }
+    },
 
     // If we need pagination
     pagination: {
